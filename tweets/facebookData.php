28ded889a246df72142985d515c7b92f43f4ab5d
mahadeva prasad <mahadevaprasad16@gmail.com>
2014-04-02 23:46:39 +0530

    first commit

---
+++ tweets/facebookData.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Home Page</title>
+	<meta name="description" content="Home page description of TRC" />
+	<meta name="keywords" content="Home page Keywords of TRC" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" /><!-- For responsive layout-->
+	<meta name="format-detection" content="telephone=no">
+	<link href="http://www.projectsouk.com/thompsonrc/css/trc.css" rel="stylesheet" type="text/css" />
+	<link href="http://www.projectsouk.com/thompsonrc/css/menu.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" />
+	
+		
+	<!-- Bootstrap css s-->
+	<link href="http://www.projectsouk.com/thompsonrc/css/bootstrap.css" rel="stylesheet">
+	<link href="http://www.projectsouk.com/thompsonrc/css/bootstrap-responsive.css" rel="stylesheet">
+	
+	
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+	
+	<!-- Le fav and touch icons -->
+    <!-- Commented out - update these with your own files -->
+    <!--
+      <link rel="shortcut icon" href="ico/favicon.ico">
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+    -->
+		
+</head>
+
+
+<body>
+	<div>
+		<iframe frameborder="0" scrolling="no" class="IframefacebookFeeds" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fthompson.road.clinic&amp;width=292&amp;height=427&amp;show_faces=false&amp;colorscheme=light&amp;stream=true&amp;show_border=false&amp;header=true">
+		</iframe>
+	</div>
+</body>
+</html>	
